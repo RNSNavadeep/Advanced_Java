23356bd56572c35533731336c4c38ec3aab55ec4
RNSNavadeep <24pa1a12k5@vishnu.edu.in>
2026-07-06 18:20:42 +0530

    Delete new.dart

--- new.dart
+++
@@ -1,8 +0,0 @@
-void main()
-{
-    var list=[{"Name":"John","Age":30},{"Name":"Jane","Age":25}];
-    for(var i=0;i<list.length;i++)
-    {var x=list[i];
-    print("name=${x['Name']},age=${x['Age']}");
-    }
-}
